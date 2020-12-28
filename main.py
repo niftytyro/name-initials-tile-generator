e85c7c44237161037e008a8ac7dc89627bf12357
Udasi Tharani <udasitharani@gmail.com>
2020-12-29 01:51:45 +0530

    added save utility and improved name initials algorithm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageDraw, ImageFont
 
-def generate_tile(initials, bgColor=(15,15,15), fgColor='white'):
+def generate_tile(initials, save_path, bgColor=(15,15,15), fgColor='white'):
     canvas_w, canvas_h = 400, 400
     poppinsFont = ImageFont.truetype("/home/udasitharani/Downloads/ttf/Poppins-Medium.ttf", 200)
     
@@ -10,13 +10,17 @@ def generate_tile(initials, bgColor=(15,15,15), fgColor='white'):
     text_w, text_h = draw.textsize(initials, font=poppinsFont)
     draw.text(((canvas_w-text_w)/2, (canvas_h-text_h)/3), initials, fill=fgColor, font=poppinsFont)
 
-    return canvas
+    canvas.save(save_path)
 
 def generate_initials_from_string(text):
-    return "".join([initial.capitalize()[0] for initial in text.split(" ") ])
+    split_text = text.split(" ")
+    if(len(split_text)):
+        if(len(split_text)==1):
+            return split_text[0][0].capitalize()
+        else:
+            return "".join([split_text[0][0].capitalize(), split_text[-1][0].capitalize()])
 
 initials = generate_initials_from_string(input("Enter you name:\n"))
 print(initials)
 
-canvas = generate_tile(initials, bgColor=(250,250,250), fgColor='black')
-canvas.show()
+generate_tile(initials, "/home/udasitharani/Pictures/initials.png", bgColor=(250,250,250), fgColor='black')
