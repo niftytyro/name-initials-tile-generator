6576232009b346d4402e2e1b7eecaa74e927bfd9
Udasi Tharani <udasitharani@gmail.com>
2020-12-31 01:55:36 +0530

    fix package to work with cli

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="nit-udasitharani",
-    version="0.0.1",
+    version="0.0.4",
     author="Udasi Tharani",
     author_email="udasitharani@gmail.com",
     description="A tool to generate tiles from name initials.",
